ed0f9b3a35d8f075d669d25001b66c2493ed129d
Gopal <barge.gopal@gmail.com>
2019-03-26 00:55:20 -0700

    pipeline code added

---
+++ src/test/java/com/techlearner/society/mysociety/controller/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.techlearner.society.mysociety.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloControllerTest {
+
+    @Test
+    public void getHello() {
+        System.out.println("GET");
+    }
+
+    @Test
+    public void postHello() {
+        System.out.println("POST");
+
+    }
+
+    @Test
+    public void putHello() {
+        System.out.println("PUT");
+
+    }
+
+    @Test
+    public void deleteHello() {
+        System.out.println("DELETE");
+
+    }
+}
